bdea70032b0383f67699daeda067e5f855ea8b5b
PatrykCebula <patryk20050@o2.pl>
2024-03-04 13:32:57 +0100

    added CheckForWin()

--- kikskibidi/kikskibidi/MainPage.xaml.cs
+++ kikskibidi/kikskibidi/MainPage.xaml.cs
@@ -21,13 +21,45 @@ namespace kikskibidi
 
         }
 
+        private bool CheckForWin(Grid grid)
+        {
+            List<Button> buttons = new List<Button>();
+            foreach (Button button in grid.Children)
+                buttons.Add(button);
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (buttons[i].Text == buttons[i + 3].Text && buttons[i + 3].Text == buttons[i + 6].Text && !string.IsNullOrEmpty(buttons[i].Text))
+                {
+                    DisplayWin();
+                    return true;
+                }
+
+                if (buttons[i*3].Text == buttons[i *3+1].Text && buttons[i *3+1].Text == buttons[i *3+2].Text && !string.IsNullOrEmpty(buttons[i*3].Text))
+                {
+                    DisplayWin();
+                    return true;
+                }
+            }
+            if (buttons[0].Text == buttons[4].Text && buttons[4].Text == buttons[8].Text && !string.IsNullOrEmpty(buttons[4].Text))
+            {
+                DisplayWin();
+                return true;
+            }
+            if (buttons[2].Text == buttons[4].Text && buttons[4].Text == buttons[6].Text && !string.IsNullOrEmpty(buttons[4].Text))
+            {
+                DisplayWin();
+                return true;
+            }
+            return false;
+        }
         private void DisplayWin()
         {
             if (turn)
                 DisplayAlert("Koniec gry!", "Wygrywa x!", "ciezko pracuje");
             else
-                DisplayAlert("Koniec gry!", "Wygrywa x!", "ciezko pracuje");
-
+                DisplayAlert("Koniec gry!", "Wygrywa o!", "ciezko pracuje");
         }
+        
     }
 }
