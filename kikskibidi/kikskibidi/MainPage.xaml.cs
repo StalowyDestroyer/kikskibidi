14f87ca495446a82b7e2a47fa5431c6aef135638
Bart1251 <bartlomiej.wiecek@vp.pl>
2024-03-04 13:31:57 +0100

    add CreateBoard function

--- kikskibidi/kikskibidi/MainPage.xaml.cs
+++ kikskibidi/kikskibidi/MainPage.xaml.cs
@@ -14,11 +14,58 @@ namespace kikskibidi
         public MainPage()
         {
             InitializeComponent();
+            CreateBoard();
+        }
+
+        private void CreateBoard()
+        {
+            MainGrid.Children.Clear();
+            MainGrid.ColumnSpacing = 0;
+            MainGrid.RowSpacing = 0;
+            turn = true;
+            TurnLabel.Text = "Kolejka: x";
+
+            for (int i = 0; i < 3; i++)
+            {
+                MainGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(0, GridUnitType.Auto) });
+                for(int j = 0; j < 3; j++)
+                {
+                    MainGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength (0, GridUnitType.Auto) });
+                    Grid grid = new Grid()
+                    {
+                        RowSpacing = 0,
+                        ColumnSpacing = 0,
+                    };
+                    Grid.SetRow(grid, i);
+                    Grid.SetColumn(grid, j);
+                    MainGrid.Children.Add(grid);
+                    for(int k = 0; k < 3; k++)
+                    {
+                        grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(0, GridUnitType.Auto) });
+                        for(int l = 0; l < 3; l++)
+                        {
+                            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(0, GridUnitType.Auto) });
+                            Button button = new Button()
+                            {
+                                WidthRequest = 38,
+                                HeightRequest = 38,
+                                Margin = new Thickness(1),
+                                ClassId = "enabled",
+                                BorderColor = Color.White,
+                            };
+                            Grid.SetRow(button, k);
+                            Grid.SetColumn(button, l);
+                            button.Clicked += Move;
+                            grid.Children.Add(button);
+                        }
+                    }
+                }
+            }
         }
 
         private void Restart(object sender, EventArgs e)
         {
-
+            CreateBoard();
         }
     }
 }
